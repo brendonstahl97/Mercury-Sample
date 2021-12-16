ef87c60a2511d80f40c60dbc162d347098eea9e0
brendonstahl97 <brendonstahl97@gmail.com>
2021-12-15 23:51:44 -0500

    Finalized formatting and added images

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-$(function () {
+$(() => {
     $('[data-toggle="tooltip"]').tooltip();
     $('#pickup-modal').modal('show');
 
+    $(".img-upload")[0].onchange = changeFilename;
+
 });
+
+function changeFilename() {
+    let filename = this.value;
+
+    const lastIndex = filename.lastIndexOf("\\");
+    if (lastIndex >= 0) {
+       filename = filename.substring(lastIndex + 1);
+    }
+    $(".filename")[0].innerHTML = filename;
+}
